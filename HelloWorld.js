fa09633c5a2a15a3e02ac8913e4125eb5349c9cb
DuongDo-InterSystems <Duong.Do@intersystems.com>
2019-06-04 16:21:26 -0400

    Move connection message up

--- HelloWorld.js
+++ HelloWorld.js
@@ -16,12 +16,12 @@ function main()
 
     // Create connection to InterSystems IRIS
     const connection = irisnative.createConnection({host: ip, port: port, ns: namespace, user: username, pwd: password})
-    
+    console.log("Hello World! You have successfully connected to InterSystems IRIS.")
+
     // Create an InterSystems IRIS native object
     const irisNative = connection.createIris()
-    console.log("Hello World! You have successfully connected to InterSystems IRIS via Node.js.")
 
-    // Store data natively into a global
+    // Store data natively into a global using the InterSystems IRIS native object
     irisNative.set(8888, "^testglobal", "1");
     globalValue = irisNative.get("^testglobal", "1");
     console.log("The value of ^testglobal(1) is " + globalValue);
